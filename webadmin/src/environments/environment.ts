27c5b6137fad87e913c61017f0c7ba3c11e0c799
HiKeean <hizkiaalbertianhal@gmail.com>
2026-09-11 14:45:16 +0700

    perbaikan prontend

--- webadmin/src/environments/environment.ts
+++ webadmin/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   useEmulators: false,
   firebaseConfigured: false,
   api: {
-    baseUrl: 'http://localhost:8080/api/v1',
+    baseUrl: 'http://api.klarfinance.hizkialb.xyz/api/v1',
     // baseUrl: 'https://your-api-domain.com/api',
     apiKey: '8815d46823660294132c4b785b4b2cccd7b3d7e9',
     secretKey: '404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970'
